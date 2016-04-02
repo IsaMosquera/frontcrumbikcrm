bd22913e705bcb3e49c53aaacd33ea076c358d72
Zarkys <zarkys.salas@gmail.com>
2016-04-02 18:39:48 -0430

    Jquery Main

---
+++ app/assets/javascripts/jQuery_Main.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+
+   //MENU TOGGLE
+  
+   
+    $('#tree_el').animate({
+           left: "0px",
+           duration: 600
+        });
+  
+//$('#animarte')
+  //  .velocity({ width: 75 }, 1500)
+   // .velocity({ height: 0 }, 1250);
+});
+
