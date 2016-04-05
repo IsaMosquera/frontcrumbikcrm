590cdde8eceb60037cbd9bce9c9881dc98fcce2a
Isabel Mosquera <imosq91@gmail.com>
2016-04-05 15:56:10 -0430

    algo del correo

--- app/assets/javascripts/newCustomer.js
+++ app/assets/javascripts/newCustomer.js
@@ -114,7 +114,7 @@ var table = $("#TablaCliente").shieldGrid({
 
                             $.ajax({
                                 type: "POST",
-                                url: "http://localhost:3000/api/v1/customers",
+                                url: "http://127.0.0.1:3000/api/v1/customers",
                                
                                 dataType: "json",
                                 data: newItem,
